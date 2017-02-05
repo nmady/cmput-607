afa39f39bf3dd8c088ee9f66cbcbd5fc183944a5
Nadia Ady <nmady@ualberta.ca>
2017-02-05 15:18:18 -0700

    Save working general value functions version before screwing it up with plots.

--- general_value_functions.py
+++ general_value_functions.py
@@ -8,9 +8,9 @@ from fractions import Fraction
 
 
 class Question(object):
-    def __init__(self, policy, cumulant, continuation_gamma):
-        self.policy = policy
-        self.cumulant = cumulant
+    def __init__(self, policy_keeper, cumulant, continuation_gamma):
+        self.policy_keeper = policy_keeper
+        self.cumulant = cumulant # is a function which takes sensor_reading, on_features, and timestep
         self.continuation_gamma = continuation_gamma
 
 
@@ -72,11 +72,9 @@ class GeneralValueFunctionTDLambda(GeneralValueFunction):
         self.delta = self.cumulant + self.continuation_gamma * sum(self.weights[self.features_new]) \
                      - sum(self.weights[self.features_old])
 
-        self.eligibility_trace *= (self.trace_decay * self.continuation_gamma)
-        self.eligibility_trace[self.features_old] += 1
+        self.set_eligibility_trace()
 
         self.weights += ((self.learning_rate * self.delta) * self.eligibility_trace)
-        # print(self.weights[[13, 21]])
 
         self.features_old = self.features_new
 
@@ -85,51 +83,146 @@ class GeneralValueFunctionTDLambda(GeneralValueFunction):
         self.cumulant = None
         self.continuation_gamma = None
 
+    def set_eligibility_trace(self):
+        self.eligibility_trace *= (self.trace_decay * self.continuation_gamma)
+        self.eligibility_trace[self.features_old] += 1
 
-class GeneralValueFunctionGTD(GeneralValueFunction):
-    def __init__(self):
-        GeneralValueFunction.__init__(self, num_features, initial_on_features, question,
+
+class GeneralValueFunctionGTD(GeneralValueFunctionTDLambda):
+    def __init__(self, num_features, initial_on_features, question,
+                                      initial_weights='random',
+                                      trace_decay=0,
+                                      learning_rate=None):
+        GeneralValueFunctionTDLambda.__init__(self, num_features, initial_on_features, question,
                                       initial_weights=initial_weights,
                                       trace_decay=trace_decay,
                                       learning_rate=learning_rate)
 
         # Used for off-policy predictions, so we need a policy.
-        assert (self.question.policy is not None)
+        assert (self.question.policy_keeper is not None)
 
         self.importance_ratio = None  # rho; probability of action under target policy divided by prob under behaviour policy
 
-    def learn(self, action):
-        pass
+    def update(self, sensor_reading, features_new, timestep, action, prob_under_behaviour):
+        GeneralValueFunction.update(self, sensor_reading, features_new, timestep)
+
+        #TODO: Should the state I'm passing be the sensor_reading or the feature representation of it (features_new)?
+        prob_under_target = self.question.policy_keeper.get_probability(sensor_reading, action)
+        self.importance_ratio = Fraction(prob_under_target)/Fraction(prob_under_behaviour)
+
+    def learn(self):
+        GeneralValueFunctionTDLambda.learn(self)
+        self.importance_ratio = None
+
+    def set_eligibility_trace(self):
+        self.eligibility_trace *= (self.trace_decay * self.continuation_gamma)
+        self.eligibility_trace[self.features_old] += 1
+        self.eligibility_trace *= float(self.importance_ratio)
 
 
-class PolicyTracker(object):
+class PolicyKeeper(object):
     # Yay for fractions!
 
-    def __init__(self):
-        self.policy_probs = {}
-        self.policy_views = {}
+    def __init__(self, policy_probs=None, policy_views=None, policy_func=None):
+        self.policy_func = policy_func
+        if policy_probs:
+            assert(type(policy_probs) == dict)
+            self.policy_probs = policy_probs
+        else:
+            self.policy_probs = {}
+
+        if policy_views:
+            assert(type(policy_views) == dict)
+            self.policy_views = policy_views
+        else:
+            self.policy_views = {}
 
     def add_to_policy(self, state, action):
         if state not in self.policy_probs:
-            self.policy_probs[state] = {action: Fraction(1)}
-            self.policy_views[state] = 1
-        else:
-            self.policy_views[state] += 1
-            k = self.policy_views[state]
+            self.policy_probs[state] = {action: Fraction(0)}
 
-            for a in self.policy_probs[state]:
-                self.policy_probs[state][a] *= Fraction(k - 1, k)
-            if action in self.policy_probs[state]:
-                self.policy_probs[state][action] += Fraction(1, k)
-            else:
-                self.policy_probs[state][action] = Fraction(1, k)
+        if state not in self.policy_views:
+            self.policy_views[state] = 0
+
+        self.policy_views[state] += 1
+        k = self.policy_views[state]
+
+        for a in self.policy_probs[state]:
+            self.policy_probs[state][a] *= Fraction(k - 1, k)
+        if action in self.policy_probs[state]:
+            self.policy_probs[state][action] += Fraction(1, k)
+        else:
+            self.policy_probs[state][action] = Fraction(1, k)
 
     def get_probability(self, state, action):
-        if state not in self.policy_probs or action not in self.policy_probs[state]:
+        if self.policy_func:
+            # then we know the policy perfectly and can return the probability for any pair
+            return self.policy_func(state, action)
+
+        # Otherwise, we are approximating the policy and return a guess from our table.
+        elif state not in self.policy_probs or action not in self.policy_probs[state]:
             return 0
         else:
             return self.policy_probs[state][action]
 
 
+class Verifier(object):
+
+    def __init__(self, gvf, initial_timestep, wait_time=100):
+        """
+
+        :type gvf: GeneralValueFunction
+        """
+        self.gvf = gvf
+        self.initial_timestep = initial_timestep
+        self.wait_time = wait_time
+
+        self.last_timestep = None
+        self.timestep_return_is_for = initial_timestep
+
+        self.gammas = []
+        self.cumulants = []
+
+    def update(self, timestep):
+        assert(timestep != self.last_timestep)
+        assert(gvf.continuation_gamma is not None)
+        assert(gvf.cumulant is not None)
+        self.last_timestep = timestep
+        self.gammas.append(gvf.continuation_gamma)
+        self.cumulants.append(gvf.cumulant)
+        if len(self.gammas) > self.wait_time:
+            self.timestep_return_is_for += 1
+            self.gammas.pop(0)
+            self.cumulants.pop(0)
+
+    def get_idealized_return(self):
+        if len(self.gammas) < self.wait_time:
+            return None
+        else:
+            # idealized_return = 0
+            # for k in range(self.wait_time):
+            #    idealized_return += numpy.product(self.gammas[:k]) * self.cumulant[k]
+
+            return self.timestep_return_is_for, sum([numpy.product(self.gammas[:k]) * self.cumulants[k] for k in range(self.wait_time)])
+
+
+
+
 if __name__ == '__main__':
-    pass
+
+    gvf = GeneralValueFunctionTDLambda(num_features=1, initial_on_features=[0], question=None)
+
+    test_verifier = Verifier(gvf, 0, wait_time=30)
+
+    for t in range(31):
+        gvf.cumulant = 1
+        gvf.continuation_gamma = 0.8
+        test_verifier.update(t)
+
+        print('self.gammas: ' + str(test_verifier.gammas))
+        print('self.cumulants: ' + str(test_verifier.cumulants))
+        print('idealized return: ' + str(test_verifier.get_idealized_return()))
+
+
+
+
--- robot_do_gvfs.py
+++ robot_do_gvfs.py
@@ -2,17 +2,42 @@ import fake_robot as robot
 import general_value_functions as gvfs
 import simple_tiles
 import simple_kanerva
-import sys
+import sys, signal
+import random
+from fractions import Fraction
 
 import numpy
 
+
+random.seed(1)
+
+
+def random_walk_policy(sensor_readings, on_features):
+    return random.choice([412, 512, 612])
+
+
 if __name__ == '__main__':
+
+    def signal_handler(signal, frame):
+        # http://stackoverflow.com/questions/18994912/ending-an-infinite-while-loop
+        print("\n Program exiting gracefully")
+        sys.exit(0)
+
+
+    signal.signal(signal.SIGINT, signal_handler)
+
     myRobot = robot.Robot()
 
     t = 0
 
     sensor_readings = myRobot.get_readings()
     corners = myRobot.get_sensorspace_corners()
+
+    # Here we set our behaviour policy!
+    behaviour_policy = random_walk_policy
+    behaviour_policy_keeper = gvfs.PolicyKeeper(policy_func=(lambda s,a: Fraction(1,3) if a in [412, 512, 612] else Fraction(0)))
+
+    # Here we set up our initial feature vector, x!
 
     if len(sys.argv) > 1 and sys.argv[1] == 'tilecoding':
         num_bins = 10
@@ -25,18 +50,62 @@ if __name__ == '__main__':
         coding = simple_kanerva.KanervaCoding(corners[0], corners[1], num_features, random_seed=10)
         on_features = coding.get_x(sensor_readings, num_on_features, distance_metric='hamming')
 
-    print('init',sensor_readings,on_features)
+    # Here we set up our demons!
 
-    #meant to ask how many steps uptil the temp is 40
-    question1 = gvfs.Question(None, (lambda x,y,z: 0 if x[2]==40 else 1), (lambda x,y,z: 0 if x[2]==40 else 1))
+    # meant to ask how many steps until the temp is 40
+    question1 = gvfs.Question(None, cumulant=(lambda x,y,z: 0 if x[2] == 40 else 1), continuation_gamma=(lambda x,y,z: 0 if x[2] == 40 else 1))
+
+    # meant to ask how many steps until the position is > 550 if we were to always pick 612
+    # policy2 = gvfs.PolicyKeeper(policy_probs={[0,0,412]:{612:Fraction(1)},[0,0,512]:{612:Fraction(1)},[0,0,612]:{612:Fraction(1)}})
+    policy2 = gvfs.PolicyKeeper(policy_func=(lambda s,a: Fraction(1) if a == 612 else Fraction(0)))
+    question2 = gvfs.Question(policy2, (lambda x,y,z: 1), (lambda x,y,z: 0 if x[0]>550 else 1))
+
+    # meant to ask how many steps until the position is > 550 (on-policy)
+    question3 = gvfs.Question(None, (lambda x, y, z: 0 if x[0] > 550 else 1),
+                              (lambda x, y, z: 0 if x[0] > 550 else 1))
+
+    # meant to ask the expected value of the position over the next ~10 timesteps
+    question4 = gvfs.Question(None, lambda x,y,z: x[0], lambda x,y,z: 0.9)
+
+
+    demons = []
 
     demon1 = gvfs.GeneralValueFunctionTDLambda(initial_on_features=on_features,
                                                question=question1,
                                                num_features=num_features,
                                                initial_weights=numpy.zeros(num_features),
                                                trace_decay=0.2)
+    demons.append(demon1)
 
-    for t in range(1, 10000+1):
+    demon2 = gvfs.GeneralValueFunctionGTD(initial_on_features=on_features,
+                                          question=question2,
+                                          num_features=num_features,
+                                          initial_weights=numpy.zeros(num_features),
+                                          trace_decay=0)
+    demons.append(demon2)
+
+    demon3 = gvfs.GeneralValueFunctionTDLambda(initial_on_features=on_features,
+                                               question=question3,
+                                               num_features=num_features,
+                                               initial_weights=numpy.zeros(num_features),
+                                               trace_decay=0.2)
+    demons.append(demon3)
+
+    demon4 = gvfs.GeneralValueFunctionTDLambda(initial_on_features=on_features,
+                                               question=question4,
+                                               num_features=num_features,
+                                               initial_weights=numpy.zeros(num_features),
+                                               trace_decay=0.2)
+
+    demons.append(demon4)
+
+    # Now we start the episode.
+
+    for t in range(1, 1000+1):
+
+        # take action a_t!
+        action_old = behaviour_policy(sensor_readings, on_features)
+        myRobot.take_action(action_old)
 
         # take new sensor readings (get s' = s_{t+1})
         sensor_readings = myRobot.get_readings()
@@ -46,11 +115,17 @@ if __name__ == '__main__':
             on_features = coding.get_x(sensor_readings, num_on_features, distance_metric='hamming')
 
         # update the state space
-        demon1.update(sensor_readings, on_features, t)
-        demon1.learn()
-
+        for d in demons:
+            if d.__class__ == gvfs.GeneralValueFunctionGTD:
+                #TODO: Decide on what state being passed to get_probability should be!
+                d.update(sensor_readings, on_features, t, action_old, behaviour_policy_keeper.get_probability(sensor_readings, action_old))
+                # print('Action: ' + str(action_old) + ' Rho: ' + str(d.importance_ratio))
+            else:
+                d.update(sensor_readings, on_features, t)
+            d.learn()
 
     # What is the answer at the following states?
+
     if len(sys.argv) > 1 and sys.argv[1] == 'tilecoding':
         print('35: ' + str(simple_tiles.get_x([0,0,35], corners[0], corners[1], num_bins)))
         feat_35 = simple_tiles.get_x([0,0,35], corners[0], corners[1], num_bins)
@@ -58,9 +133,20 @@ if __name__ == '__main__':
         print(sum(demon1.weights[feat_35]))
         print(sum(demon1.weights[feat_40]))
     else:
+        # answers for question 2
+        feat412 = coding.get_x([412, 0, 40], num_on_features=num_on_features, distance_metric='hamming')
+        feat512 = coding.get_x([512, 0, 40], num_on_features=num_on_features, distance_metric='hamming')
+        feat612 = coding.get_x([612, 0, 40], num_on_features=num_on_features, distance_metric='hamming')
+        print(sum(demon2.weights[feat412]))
+        print(sum(demon2.weights[feat512]))
+        print(sum(demon2.weights[feat612]))
+        print
+        # answers for question 1
         print(sum(demon1.weights[coding.get_x([0, 0, 35], num_on_features=num_on_features, distance_metric='hamming')]))
         feat_35 = coding.get_x([0, 0, 35], num_on_features=num_on_features, distance_metric='hamming')
         print(sum(demon1.weights[coding.get_x([0, 0, 40], num_on_features=num_on_features, distance_metric='hamming')]))
         feat_40 = coding.get_x([0, 0, 40], num_on_features=num_on_features, distance_metric='hamming')
     print(set(feat_35).symmetric_difference(set(feat_40)))
     print(demon1.weights[list(set(feat_35).symmetric_difference(set(feat_40)))])
+
+
